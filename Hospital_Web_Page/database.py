80d846e7d90548730b3e7540a23f05db4659fade
Ubeyd Gür <ubeydgur41@gmail.com>
2024-05-14 21:20:54 +0300

    Add doctors, admins, appointment, report tables to the database

--- Hospital_Web_Page/database.py
+++ Hospital_Web_Page/database.py
@@ -4,7 +4,8 @@ conn = sqlite3.connect('hospital.db')
 
 c = conn.cursor()
 
-'''c.execute("""CREATE TABLE patients(
+'''
+c.execute("""CREATE TABLE patients(
     patientID INTEGER PRIMARY KEY NOT NULL, 
     firstname TEXT NOT NULL,
     surname TEXT NOT NULL,
@@ -14,8 +15,39 @@ c = conn.cursor()
     address TEXT NOT NULL,
     password TEXT NOT NULL
     )""")
-'''
 
+c.execute("""CREATE TABLE doctors(
+    doctorID INTEGER PRIMARY KEY NOT NULL, 
+    firstname TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    specialty TEXT NOT NULL,
+    workplace TEXT NOT NULL,
+    password TEXT NOT NULL
+    )""")
+
+c.execute("""CREATE TABLE admins(
+    adminID INTEGER PRIMARY KEY NOT NULL, 
+    password TEXT NOT NULL
+    )""")
+
+c.execute("""CREATE TABLE appointment(
+    appointmentID INTEGER PRIMARY KEY NOT NULL, 
+    date TEXT NOT NULL,
+    time INTEGER NOT NULL,
+    doctorID INTEGER,
+    patientID INTEGER,
+    FOREIGN KEY (patientID) REFERENCES patients(patientID),
+    FOREIGN KEY (doctorID) REFERENCES doctors(doctorID)
+    )""")
+
+c.execute("""CREATE TABLE report(
+    reportID INTEGER PRIMARY KEY NOT NULL, 
+    date TEXT NOT NULL,
+    link TEXT NOT NULL,
+    patientID INTEGER,
+    FOREIGN KEY (patientID) REFERENCES patients(patientID)
+    )""")
+'''
 #c.execute("INSERT INTO patients VALUES ('0', 'ubeyd', 'gur', '05.04.2003', 'M', '05511643357', 'address', '1')") # tabloya veri ekleme
 
 #c.execute("PRAGMA table_info(patients)") # sütunları döndürür
